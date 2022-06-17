edd607c3161083b3a126805cf822ab0ef9013862
Andy Silitonga <40969170+AndyNotfound@users.noreply.github.com>
2022-06-17 09:51:27 +0700

    Update exOne.cpp

--- exOne.cpp
+++ exOne.cpp
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <conio.h>
 
-//int main (){
-//	printf("Halo, \nSaya adalah Program komputer\n");
-//	printf("Saya bisa menyelesaikan semua problem Anda\n");
-//	printf("Mau tau caranya...?\n\n");
-//	getche ();
-//}
-
 using namespace std;
 
 int main (int argc, char *argv[]){
